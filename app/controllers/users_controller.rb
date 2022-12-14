cce1d83bf91511d0a268275f007da12e25e6e3af
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-15 00:02:07 +0800

    changed webpcker version on gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
-
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:success] = "This user was successfully removed."
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "This user was successfully removed."
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
