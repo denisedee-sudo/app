46510d2891897f644ae282c6d8b79c1cb5e3e255
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-19 04:55:10 +0800

    readded show link

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # comment the line below back for render.com
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+# workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
