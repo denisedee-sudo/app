03bb2734beb48687e26b4e3b5da45b39a1458f02
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-19 02:52:24 +0800

    changed uname to name because render is annoying

--- db/schema.rb
+++ db/schema.rb
@@ -7,7 +7,7 @@ ActiveRecord::Schema.define(version: 2022_12_16_163621) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "uname"
+    t.string "name"
     t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #   
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([{ uname: "Ise", password: BCrypt::Password.create("password"), first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, { uname: "Derp", password_digest: "dankmemes", first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
+users = User.create([{ name: "Ise", password: BCrypt::Password.create("password"), first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, { name: "Derp", password_digest: "dankmemes", first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
 toys = Toy.create([{ name: 'Lego set', description: 'Modular building blocks' }])
