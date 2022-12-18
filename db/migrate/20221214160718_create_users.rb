819d4712ce7c67ec35a4f95a5abe3dc4cf2ac292
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-19 01:24:01 +0800

    added password hash

--- db/migrate/20221214160718_create_users.rb
+++ db/migrate/20221214160718_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
       t.string :email
-
+      t.bigint :phone
+      t.integer :admin
       t.timestamps
     end
   end
