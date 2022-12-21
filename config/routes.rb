ab456695392d000e95de612b3200aeb1f796d1bf
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-21 18:42:08 +0800

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users
   get "/signup" => "users#new"
   post "/signup" => "users#create"
-  root "users#index"
+  root "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
