d1f29b0a8230df538fa00ee74daf220e39b08c1b
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-23 00:50:15 +0800

    made shared view folder

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,3 +1,7 @@
 class Toy < ApplicationRecord
     belongs_to :user
+    has_one_attached :image
+    default_scope -> { order(created_at: :desc) }
+    validates :user_id, presence: true
+    # validates :content, presence: true, length: { maximum: 140 }
 end
