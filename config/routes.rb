9ff19863bcc06f9a6c8bf8998e5ec5cb0603e426
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-22 21:52:11 +0800

    session id error persistent

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :toys
   resources :users
-  get '/login' => "pages#new"
-  post '/login' => "pages#create"
-  delete '/logout' => 'pages#destroy' 
-  get '/about' => "pages#about"
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+  delete '/logout' => 'sessions#destroy' 
+  get '/about' => "sessions#about"
   get "/signup" => "users#new"
   post "/signup" => "users#create"
-  root "pages#index"
+  root "sessions#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
