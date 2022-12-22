f71d57235712eaa606d0c82a872371394fbef153
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-22 21:37:31 +0800

    changed helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
     helper_method :current_user 
  
     def current_user 
-    @current_user ||= User.find(page[:user_id]) if page[:user_id] 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
     end 
     def require_user 
         redirect_to '/login' unless current_user 
-    end  
+      end  
 end
