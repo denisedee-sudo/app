6044e2cfa75e85439d0adfa96b2d210fcc310951
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-26 21:38:59 +0800

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 users = User.create([
-    { name: "Ise", password: BCrypt::Password.create("password"), first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, 
-    { name: "MX", password: BCrypt::Password.create("bab"), first_name: "Mischka" , last_name: "Snackchez", email: "mx@email.com", phone: 9760507982, admin: 1 }, 
-    { name: "Derp", password_digest: BCrypt::Password.create("dankmemes"), first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
+    { name: "Ise", password: "password", first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, 
+    { name: "MX", password: "bab", first_name: "Mischka" , last_name: "Snackchez", email: "mx@email.com", phone: 9760507982, admin: 1 }, 
+    { name: "Derp", password_digest: "dankmemes", first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
 toys = Toy.create([{ name: 'Lego set', description: 'Modular building blocks', user_id: 1 },
     { name: 'Bucky Balls', description: 'Magnetic metal balls', user_id: 2 }
     ])
